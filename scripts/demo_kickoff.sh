3c6e1b291251d255d55f25c7143b1ba301be086e
Eric Cobb <ericcobb@elastic.co>
2025-09-22 09:19:52 -0500

    Added comments to make variables more clear

--- scripts/demo_kickoff.sh
+++ scripts/demo_kickoff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-TARGET_HOST=''
-ROOTDIR=''
-SSH_USER=''
+TARGET_HOST='' # The hostname or IP of the host to which this script will attempt to login and start triggering alerts
+ROOTDIR='' # Base directory from which all of these alert triggers will be run on the target host
+SSH_USER='' # The user account on the TARGET_HOST
 
 ssh $SSH_USER@$TARGET_HOST "/usr/bin/find . -exec /bin/sh -c $ROOTDIR/scripts/exfill.sh \; -quit"
 
--- scripts/exfill.sh
+++ scripts/exfill.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOTDIR='' # Base directory of this repo
+ROOTDIR='' # Base directory from which all of these alert triggers will be run on the target host
 HOMEDIR='' # Your home directory where your ssh keys are
 EXFILL_TARGET='' # Where the scp command will attempt to "exfill" the data
 FILE='/dev/shm/totally_not_exfill.tar.gz'
