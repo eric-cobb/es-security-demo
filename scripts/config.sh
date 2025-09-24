49aa1b5241cfd5393f3dbf18dca3000519246e4c
Eric Cobb <ericcobb@elastic.co>
2025-09-23 21:46:53 -0500

    Added additional variables to reduce manual work setting up the repo

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,12 +1,42 @@
-# Base directory from which all of these alert triggers 
-#will be run on the target host
-ROOTDIR='' 
+######################
+## TARGET VARIABLES ##
+######################
+# The hostname or IP of the host to which this script will attempt
+# to login and start triggering alerts
+TARGET_HOST=''
 
-# Your home directory where your ssh keys are
-HOMEDIR='' 
+# Base directory from which all of these alert triggers
+# will be run on the target host
+TARGET_HOST_ROOTDIR=''
 
-# Where the scp command will attempt to "exfill" the data
-EXFILL_TARGET='' 
+# The user SSH account on the TARGET_HOST that the demo_start.sh
+# script will use to ssh into the TARGET_HOST from the local machine
+TARGET_HOST_SSH_USER=''
+
+
+######################
+## EXFILL VARIABLES ##
+######################
+# The host that the scp command will attempt to "exfill" the data to
+EXFILL_TARGET=''
+
+# The user SSH account on the EXFILL_TARGET
+EXFILL_SSH_USER=''
+
+# The user account SSH private key on the TARGET_HOST that will
+# be used to scp the 'exfill' data to the EXFILL_TARGET
+EXFILL_SSH_KEY=''
+
+
+#######################
+##  LOCAL VARIABLES  ##
+#######################
+# The local machine private key; used by the demo_setup.sh and demo_start.sh 
+# scripts on local machine to ssh into the TARGET_HOST
+LOCAL_SSH_KEY=''
+
+# Your home directory
+#HOMEDIR=''
 
 # This is the file that will be exfilled
-FILE='/dev/shm/totally_not_exfill.tar.gz' 
+FILE='/dev/shm/totally_not_exfill.tar.gz'
